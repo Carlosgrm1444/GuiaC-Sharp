7bb19d546f9e3efa933a23adeff4630ce3adb9a4
Carlosgrm1444 <117303471+Carlosgrm1444@users.noreply.github.com>
2023-11-08 02:46:25 -0500

    terminando operadores aritmeticos

--- Proyectos/Proyecto1/Program.cs
+++ Proyectos/Proyecto1/Program.cs
@@ -7,14 +7,21 @@
             int entero = 1;
             double puntoDecimal = 1.234;
             char caracter = 'a';
-            string texto = "Un texto";
             bool booleano = true;
 
             int variable1 = 1;
             int variable2; 
             variable2 = 2;
 
-            Console.WriteLine(entero);
+            string texto = "Un texto";
+
+            int num1 = 7;
+            int num2 = 4;
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine(num1 / num2);
+            Console.WriteLine(num1 % num2);
         }
     }
 }
